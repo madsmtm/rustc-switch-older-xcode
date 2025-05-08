222fbeb49b8854ae760709e0403326824bf1d85d
Mads Marquart <mads@marquart.dk>
2025-05-08 18:58:02 +0200

    Add README, use Xcode 14.3.1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Minimized reproduction for [rust-lang/rust#140686](https://github.com/rust-lang/rust/issues/140686)
+
+Run with:
+```sh
+./repro.sh
+```
+
+You need to have Xcode 14.3.1 installed.
--- repro.sh
+++ repro.sh
@@ -5,6 +5,6 @@ mkdir build
 
 rustc dep.rs --out-dir ./build --target x86_64-apple-darwin -C opt-level=1 -C codegen-units=1 -Cembed-bitcode=no
 
-rustc foo.rs --out-dir ./build --target x86_64-apple-darwin --extern dep=./build/libdep.rlib '-Clinker=/Applications/Xcode 13.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld'
+rustc foo.rs --out-dir ./build --target x86_64-apple-darwin --extern dep=./build/libdep.rlib '-Clinker=/Applications/Xcode 14.3.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld'
 
 ./build/foo
